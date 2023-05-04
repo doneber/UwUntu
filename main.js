ab371d64433ed44c2a868a33fc080b7352e0ced6
Eber <doneber98@gmail.com>
2023-05-03 21:24:53 -0400

    ✨ Agrega opcion de maximizar y minimizar

--- main.js
+++ main.js
@@ -7,10 +7,15 @@ const $ = (selector, context = document) =>
 const terminalElement = $('.terminal')
 const inputElement = $('.terminal__input input')
 const outputElement = $('.terminal__output')
+const windowElement = $('.window')
 
 const terminal = new Terminal(terminalElement, inputElement, outputElement)
 terminal.loadCommands([...commands])
 
 $('.terminal__blank-space').addEventListener('click', () => {
   inputElement.focus()
+})
+
+$('.btn-window-size').addEventListener('click', () => {
+  windowElement.classList.toggle('window--maximized')
 })
